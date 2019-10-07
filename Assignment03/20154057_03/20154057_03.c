2adddb4852cfb9020b89b28b300855011369cbc4
oxo1996 <kimsm7337@gmail.com>
2019-10-08 00:23:29 +0900

    edit assignment03

--- Assignment03/20154057_03/20154057_03.c
+++ Assignment03/20154057_03/20154057_03.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int num = 1;
 
-void subset(int n, int item, int* ptr, char** argv) {
-	if (item == n + 1) {
-		printf("[%04d] " , num);
-		for (int i = 1; i <= n; i++)
-			if (ptr[i] == 1) printf("%s, ", argv[i]);
-		if (num == 1) {
-			unsigned char a = 0x9B;
-			printf("%c ", a);
-		}
+void subset(char* argv[], int argc, int count, int aindex, char* output[], int oindex){
+	if (aindex == count) {
+		printf("[%04d]", num);
 		num++;
+		for (int j = 0; j < count; j++)
+			if(j!=count-1) printf("%s, ", output[j]);
+			else printf("%s", output[j]);
 		printf("\n");
+		return;
 	}
-	else {
-		ptr[item] = 0;
-		subset(n, item + 1, ptr, argv);
-		ptr[item] = 1;
-		subset(n, item + 1, ptr, argv);
-	}
+
+	if (oindex >= argc-1)
+		return;
+
+	output[aindex] = argv[oindex];
+	subset(argv, argc, count, aindex + 1, output, oindex + 1);
+	subset(argv, argc, count, aindex, output, oindex + 1);
+}
+
+void printsubset(char* argv[], int argc, int count) {
+
+	int** output = malloc(sizeof(int) * (count));
+
+	subset(argv, argc, count, 0, output, 0);
 }
 
 int main(int argc, char* argv[]) {
-	int* ptr = malloc(sizeof(int) * argc);
 	system("chcp 437");
-	subset(argc - 1, 1, ptr, argv);
+	
+	printf("[%04d]", num);
+	printf("%c\n", 0x9B);
+	num++;
+
+	for (int i = 0; i < argc - 1; i++) {
+		argv[i] = argv[i + 1];
+	}
+
+	for (int i = 1; i <= argc-1; i++) {
+		printsubset(argv, argc, i);
+	}
 
 	printf("***---------------------------------------***\n");
 	printf("	Computer Science & Engineering\n	20154057 KIM SUNG MIN\n");
